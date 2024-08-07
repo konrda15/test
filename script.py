a3ebef7e735dd42e4ef4b46cdb59d3cd565ffc37
konrda15 <62248249+konrda15@users.noreply.github.com>
2024-08-07 03:34:10 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 summe = 0
 
 for zahl in zahlen:
-    if zahl % 2 == 0:
+    if zahl !% 2 == 0:
         summe += zahl
 
-print("Die Summe der geraden Zahlen ist:", summe)
+print("Die Summe der ungeraden Zahlen ist:", summe)
