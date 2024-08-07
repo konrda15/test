4889d7979c3a698f9149d80c906f3ceb29856cc0
konrda15 <62248249+konrda15@users.noreply.github.com>
2024-08-07 03:33:59 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,9 @@
-# Definieren einer Liste von Zahlen
 zahlen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# Initialisieren einer Variablen, um die Summe der ungeraden Zahlen zu speichern
-summe_ungerade = 0
+summe = 0
 
-# Schleife durch jede Zahl in der Liste
 for zahl in zahlen:
-    # Überprüfen, ob die Zahl ungerade ist
-    if zahl % 2 != 0:
-        # Die ungerade Zahl zur Summe hinzufügen
-        summe_ungerade += zahl
+    if zahl % 2 == 0:
+        summe += zahl
 
-# Das Ergebnis ausgeben
-print("Die Summe der ungeraden Zahlen ist:", summe_ungerade)
+print("Die Summe der geraden Zahlen ist:", summe)
